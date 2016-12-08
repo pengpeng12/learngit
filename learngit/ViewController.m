df81ff7e82f83a837d800d91eb647a31d1909930
pengpeng <18236933571@139.com>
2016-12-08 14:02:47 +0800

    Update ViewController.m

--- learngit/ViewController.m
+++ learngit/ViewController.m
@@ -84,7 +84,68 @@
          git tag -d V2.0 删除本地标签
          git push origin :refs/tags/V2.0 删除远程标签 （先删除本地再删除远程）
          
-         //
+         //git常用命令
+         git status 查看状态
+         git diff filename 查看文件有什么不同的地方：git diff readme.txt
+                                            diff --git a/readme.txt b/readme.txt
+                                            index d267a1b..0454505 100644
+                                            --- a/readme.txt
+                                            +++ b/readme.txt
+                                            @@ -1,4 +1,4 @@
+                                             ========================
+                                             Git is a version control system.
+                                             Git is free software.
+                                            -
+                                            +jkkjfksfjkjskfjksjfkddkfks^M
+         git log 查看
+         git log --pretty=oneline
+         git log --graph --pretty=oneline --abbrev -commit  查看分支的合并情况图
+         git lg 配置好的别名有时间和颜色
+         
+         git reflog 查看历史命令，可以看到版本id
+         跳转版本： 
+         git reset --hard idname 跳到指定ID的版本
+         git reset --hard "HEAD^" 跳到上一个版本
+         git reset --hard "HEAD^^" 跳到上上个版本
+         git reset --hard "HEAD~100" 跳到上100个版本
+         
+         //修改的东西 没有add 需要撤销时：
+         git checkout -- filename  
+         //修改的东西 已经add 需要撤销时：
+         git reset HEAD filename 把add到暂存区的修改丢弃
+         再用 git checkout -- filename 撤销本地的修改
+         
+         git diff HEAD -- filename 查看工作区和版本库里最新版本的区别
+         
+         //git删除文件 
+         git rm filename
+         git commit -m "描述..."
+         如果删错了
+         用git checkout -- filename 回到刚才的状态
+         
+         //分支管理
+         git checkout -b dev   创建dev分支并切换到dev分支
+         git branch   查看当前分支
+         git checkout master   在dev上完成工作并提交完后，切换回master
+         git merge dev   合并dev到master上 ，另外用 git merge --no-ff -m "描述..." dev  (这样可以看出合并记录)
+         git branch -d dev  删除dev分支
+         
+         //Bug分支 
+         正在dev分支工作还未完成无法提交，此时master分支需要修改bug
+         git stash              先"储存"工作现场
+         git checkout master    跳转至master分支 
+         git checkout -b issue-101     在master分支上创建bug分支 issue-101
+         修复bug-----完成后提交______ git add filename.m _____  git commit -m "fix bug101"
+         回到master分支合并 issue-101分支：git checkout master
+                                         git merge --no-ff -m "merged bug fix 101" issue-101
+                                         git branch -d issue-101
+         回到dev:   git checkout dev  用git status看下状态-----查看git stash list
+                    git stash pop   恢复之前"储存"的现场 （恢复时也把stash内容也删了）
+                    
+                    
+         //删除一个没有被合并过的分支：git branch -D dev-2 （强行删除） 
+                                        
+                                        
          
     */
 }
